9d07d03202b09c1d69412898d7159f7ee5141f91
silvioAndre <s.andre.marim@gmail.com>
2023-07-29 19:16:36 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("dark")
+
+    const img = document.querySelector("#profile img")
+
+     if (html.classList.contains("dark")) {
+        
+        img.setAttribute ("src", "./rocket/fotodark.jpg")
+     } else {   
+
+        img.setAttribute ("src", "./rocket/foto1.jpg")
+
+     }
+
+}
